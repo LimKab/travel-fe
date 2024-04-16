18084fe7ada2eb82ef5309fd0391e8779768db1a
limor kabrity <limorkab@gmail.com>
2024-04-16 16:28:47 +0300

    init for navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import NavBar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/home page/Home';
+import Home from './components/homepage/Home';
 import About from './components/about/About';
-import AuthorsChoice from './components/Author\'s Choice/AuthorsChoice';
+import AuthorsChoice from './components/authorsChoice/AuthorsChoice';
 
 function App() {
   return (

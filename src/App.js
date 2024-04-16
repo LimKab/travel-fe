5fff01b192f112ca01d87ab5cbe1ddacbd1751da
limor kabrity <limorkab@gmail.com>
2024-04-16 16:11:55 +0300

    style done, WIP navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import NavBar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/navbar/home page/Home';
+import About from './components/about/About';
+import AuthorsChoice from './components/Author\'s Choice/AuthorsChoice';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/authors-choice' element={<AuthorsChoice />} />
+      </Routes>
+    </Router>
   );
 }
 

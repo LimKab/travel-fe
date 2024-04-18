01435699ba1fccf5289d07ea9d96ca0338168e5f
LukavSlambert <forspacereg@gmail.com>
2024-04-18 17:12:56 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/homepage/Home';
+import Profile from './components/profile/Profile';
 import About from './components/about/About';
 import AuthorsChoice from './components/authorsChoice/AuthorsChoice';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/authors-choice' element={<AuthorsChoice />} />
       </Routes>
     </Router>

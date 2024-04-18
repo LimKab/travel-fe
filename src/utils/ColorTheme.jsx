5c093f92c6f6fcff1c1316ecc7f315dc9aa64559
limor kabrity <limorkab@gmail.com>
2024-04-18 16:52:05 +0300

    fixing colors

---
+++ src/utils/ColorTheme.jsx
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material';
+import { colors } from './colors';
+
+export const colorTheme = createTheme({
+    palette: {
+        primary: {
+            main: colors.brandDarkGreen,
+            contrastText: '#ffffff',
+        },
+        secondary: {
+            main: colors.brandSand,
+            contrastText: '#ffffff',
+        },
+        success: {
+            main: colors.brandGreen,
+        },
+        additional: {
+            brandPurple: colors.brandPurple,
+            brandSand: colors.brandSand,
+            brandWhite: colors.brandWhite,
+            brandBrownish: colors.brandBrownish,
+            brandGreen: colors.brandGreen,
+        }
+    },
+})

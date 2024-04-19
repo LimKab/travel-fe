d4d13042f9841115fb4027421d474a8fc2df99f5
LukavSlambert <forspacereg@gmail.com>
2024-04-19 15:21:03 +0300

    HF

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,4 +1,3 @@
-import Login from "../../modals/Login"
 
 function Profile() {
 
--- src/modals/Login.jsx
+++ src/modals/Login.jsx
@@ -20,7 +20,6 @@ const style = {
 
 export default function Login() {
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
     const onSubmit = async (data) => {

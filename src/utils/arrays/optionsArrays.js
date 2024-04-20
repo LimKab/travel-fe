1a76b4c07e3697698ba2d8ab4702bb55d4e91d16
limor kabrity <limorkab@gmail.com>
2024-04-20 04:35:07 +0300

    WIP generate trip

---
+++ src/utils/arrays/optionsArrays.js
@@ -0,0 +1,20 @@
+export const season = [
+    { name: 'summer' },
+    { name: 'spring' },
+    { name: 'winter' },
+    { name: 'autumn' }
+]
+
+export const experience = [
+    { name: 'calture' },
+    { name: 'nature' },
+    { name: 'shopping' },
+    { name: 'honeymoon' },
+    { name: 'food' }
+]
+
+export const budget = [
+    { name: 'low' },
+    { name: 'medium' },
+    { name: 'high' }
+]

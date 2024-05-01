78d03aed7eb0b5836a535cd6a8c696ddef5ddb7f
limor kabrity <limorkab@gmail.com>
2024-05-01 19:36:07 +0300

    cut 10 sec from video end

--- src/components/pages/homepage/VideoBackground.jsx
+++ src/components/pages/homepage/VideoBackground.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 import YouTube from 'react-youtube';
 
 class VideoBackground extends React.Component {
+    videoDuration = null; // Duration of the video
+    cutSeconds = 10; // Seconds to cut from the end of the video
+
+    onReady = (event) => {
+        // Save the reference to the player
+        this.player = event.target;
+        // Mute the video
+        this.player.mute();
+        // Fetch and save the duration of the video
+        this.videoDuration = this.player.getDuration();
+        this.player.playVideo();
+    };
+
+    onPlay = (event) => {
+        // Constantly check the time and reset if near the end
+        const checkTime = () => {
+            if (this.player.getCurrentTime() >= this.videoDuration - this.cutSeconds) {
+                this.player.seekTo(0);
+            } else {
+                setTimeout(checkTime, 500); // Check every 500ms
+            }
+        };
+        checkTime();
+    };
+
     render() {
         const opts = {
             height: '100vh',
@@ -26,14 +51,10 @@ class VideoBackground extends React.Component {
                 videoId="Atf_Af1q_5w" // Replace 'videoId' with the actual YouTube video ID
                 className="video-iframe"
                 opts={opts}
-                onReady={this._onReady}
+                onReady={this.onReady}
+                onPlay={this.onPlay}
             />
         );
-    }
-
-    _onReady(event) {
-        // access to player in all event handlers via event.target
-        event.target.playVideo();
     }
 }
 

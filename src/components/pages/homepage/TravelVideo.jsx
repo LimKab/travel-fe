c2e32a88a0ea4e4f01a4d009ccc4302ecf53a205
David Sequerra <baruchsequerra@gmail.com>
2024-05-02 05:28:21 +0300

    travel video setup

--- src/components/pages/homepage/TravelVideo.jsx
+++ src/components/pages/homepage/TravelVideo.jsx
@@ -1,22 +1,27 @@
-function TravelVideo() {
+import YouTube from 'react-youtube';
+
+function TravelVideoWrapper() {
+    const handleTouchStart = (event) => {
+    };
+
+    return <TravelVideo onHandleTouchStart={handleTouchStart} />;
+}
+
+function TravelVideo({ onHandleTouchStart }) {
+    const opts = {
+        playerVars: {
+            autoplay: 1, // Autoplay the video
+            controls: 0, // Hide video controls
+            loop: 1, // Loop the video
+            mute: 1 // Mute the video
+        }
+    };
 
     return (
-        <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
-            <video
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover'  // Ensures video covers the entire container without distortion
-                }}
-                autoPlay
-                loop
-                muted
-                playsInline // This attribute is important for autoplay in iOS web browsers
-            >
-                <source src='assets/tutorial.mp4' type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+        <div style={{ position: 'relative', maxWidth: '100vw', maxHeight: '100vh', width: '100%', height: '100%' }}>
+            <YouTube videoId="khAY1pZQdUc" opts={opts} onReady={(event) => event.target.playVideo()} />
         </div>
     );
 }
-export default TravelVideo
+
+export default TravelVideoWrapper;
